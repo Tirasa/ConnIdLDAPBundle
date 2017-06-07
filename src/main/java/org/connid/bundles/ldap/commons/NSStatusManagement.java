e3fb0e754862f36cf2a7deddfa452316ed130a04
Francesco Chicchiriccò <ilgrosso@apache.org>
2017-06-07 11:07:49 +0200

    [LDAP-24] First remove, then add

--- src/main/java/org/connid/bundles/ldap/commons/NSStatusManagement.java
+++ src/main/java/org/connid/bundles/ldap/commons/NSStatusManagement.java
@@ -59,7 +59,8 @@ public class NSStatusManagement extends AttributeStatusManagement {
 
         LOG.ok("Calling setStatus {0}", status);
 
-        final Object value = status ? getStatusAttrActiveValue() : getStatusAttrInactiveValue();
+        Object value = status ? getStatusAttrActiveValue() : getStatusAttrInactiveValue();
+        attributes.remove(getStatusAttrName());
         attributes.put(getStatusAttrName(), value);
     }
 
@@ -72,7 +73,7 @@ public class NSStatusManagement extends AttributeStatusManagement {
         final Attribute attr = attributes.get(getStatusAttrName());
         if (attr != null) {
             try {
-                final Object value = attr.get();
+                Object value = attr.get();
                 if (value != null) {
                     status ^= getStatusAttrInactiveValue().equalsIgnoreCase(value.toString());
                 }
